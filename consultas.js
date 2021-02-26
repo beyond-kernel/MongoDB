2d6db2f9230acb6168fed27fc27bc5a01572b575
Rafael Nogueira <rafanog.linux@hotmail.com>
2021-02-26 15:25:03 -0300

    atualizacao consultas e logs

--- consultas.js
+++ consultas.js
@@ -14,5 +14,6 @@ db.pokemon.find({attack: {$lt: 85 } }, {name: 1, attack: 1, _id: 0}) //$lt opera
 db.pokemon.find({ types: {$in :["Fire", "Flying"] }  }, { types: 1,name: 1, _id: 0}) //$in operador que retorna qualquer valor que bata com os valores passados 
 db.pokemon.find({ types: {$nin :["Fire", "Flying"] }  }, { types: 1,name: 1, _id: 0}) //$nin operador que retorna qualquer valor que NAO bata com os valores passados 
 db.pokemon.find({ defense: { $gt: 60, $lte: 72  }  }, { name: 1, defense: 1, _id: 0}) //combinando operadores maior que e menor e igual que bata com valores passados
-db.pokemon.find({ $or: [{defense: { $gt: 60, $lte: 72  }}, {defense: 100}]  }, { name: 1, defense: 1, _id: 0})  //combinando operadores com "ou" maior que e menor e igual que com ou que bata exatamente com valores passados
+db.pokemon.find({ $or: [{ defense: { $gt: 60, $lte: 72  }}, {defense: 100}]  }, { name: 1, defense: 1, _id: 0})  //combinando operadores com "ou" maior que e menor e igual que com ou que bata exatamente com valores passados
 db.pokemon.find({ $or: [{ attack: { $gte: 80 }, speed: {$gte: 80} }, {defense: {$gte: 80}, hp: {$gte:80 }}]  }, { name: 1, attack:1, defense: 1, _id: 0}) //combinando operadores com "ou" e "e" com maior que para que bata com valores passados
+db.pokemon.find({ $or: [{ attack: { $gte: 80 }, speed: {$gte: 80} }, {defense: {$gte: 80}, hp: {$gte:80 }}]  }, {_id: false}).sort({hp: -1}).pretty() //utilizando cursor sort para ordernar consulta connforme valor passado 1 para crescente e -1 para decrescente 
